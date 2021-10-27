f5e81e94a4b1108e141c7e64de68a94ce226cc62
Rohan <rohanzealous@gmail.com>
2021-10-27 14:32:41 -0700

    donezo

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,43 @@
 public class Dog extends Pet {
+    /*
+    Private instance variables for name, age, and breed
+    Two constructors to create a Dog (one default and one with three parameters)
+    makeNoise(): a non-static method that just prints out an appropriate animal noise for a Dog
+    toString() method to print out the Dog's name, age, and breed
+    Getters and setters for all three variables
+     */
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public Dog() {
+        super("spots", 1);
+        this.breed = "doberman";
+    }
 
     // makeNoise() method
-
+    public void  makeNoise() {
+        System.out.println("bark bark");
+    }
 
     // toString method
-
+    @Override
+    public String toString() {
+        return this.getName() + " " + this.getBreed()+ " " +this.getAge();
+    }
 
     // Getter
-
-
+    public String getBreed() {
+        return this.breed;
+    }
     // Setter
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,9 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog t = new Dog();
+        Dog q = new Dog("Riley", 3, "Golden Retriever");
+        System.out.println(q + " " + t);
+        q.setBreed("human");
+        System.out.println(q.getBreed());
     }
 }
